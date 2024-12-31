1dcfe28e97a463ec5153977d923cff6bce3fc795
???? <mhxy13867806343@users.noreply.github.com>
2024-12-31 18:08:17 +0800

    chore: 切换到 GitHub Pages 默认部署方式

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import ElementPlus from 'unplugin-element-plus/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-                                base: '/vue-vite-lol-hreo/', // 替换为你的仓库名称
+                                base: process.env.NODE_ENV === 'production' ? '/vue-vite-lol-hreo/' : '/',
                               plugins: [
     vue(),
     AutoImport({
